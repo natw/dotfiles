9eb197f13755b06259da98128af74f3751d8d977
Nat Williams <nwilliams@leapfrogonline.com>
2014-07-07 11:55:27 -0500

    move dotted files to no-dot
    so that I can actually see them.  Symlinks should still be dotted, of
    course

--- symlink.sh
+++ symlink.sh
@@ -3,17 +3,19 @@
 set -e
 
 files=(
-    .ackrc
-    .inputrc
-    .screenrc
-    .tmux.conf
-    .vim
-    .vimrc
-    .virtualenvs
-    .zsh
-    .zshrc
+    ackrc
+    hgrc
+    inputrc
+    pentadactylrc
+    screenrc
+    tmux.conf
+    vim
+    vimrc
+    # virtualenvs
+    zsh
+    zshrc
 )
 curdir=`pwd`
 for fname in $files; do
-    ln -s $curdir/$fname $HOME/$fname
+    ln -s $curdir/$fname $HOME/.$fname
 done
