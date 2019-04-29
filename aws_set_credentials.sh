d955e8b57cc064b2e29774dd0ded52bafe3488a4
Nat Williams <nat.williams@amount.com>
2019-04-28 19:49:41 -0500

    add helper script for setting creds with aws-okta

---
+++ aws_set_credentials.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euo pipefail
+
+cat <<-EOF >~/.aws/credentials
+[default]
+aws_access_key_id=${AWS_ACCESS_KEY_ID}
+aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}
+aws_session_token=${AWS_SESSION_TOKEN}
+EOF
