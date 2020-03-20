a89fdb7d44f9d46b2a14717175ecb91231afd024
Nat Williams <nat.williams@amount.com>
2020-03-20 11:23:13 -0500

    be less clever with auth
    still a WIP, haven't completely wrapped my head around exactly how
    awscli cares about config files vs env vars

--- zsh/auth.zsh
+++ zsh/auth.zsh
@@ -2,23 +2,27 @@ aopts="-a 8h -t 8h --mfa-provider GOOGLE --mfa-factor-type token:software:totp"
 
 ar () {
   local cmd=${argv:-$SHELL}
-  eval "aws-okta ${aopts} exec root -- ${SHELL} -c \"auth_name=org ${cmd}\""
+  eval "aws-okta ${aopts} exec root -- ${SHELL} -c \"${cmd}\""
 }
 
 ap () {
   local cmd=${argv:-$SHELL}
-  eval "aws-okta ${aopts} exec prod -- ${SHELL} -c \"auth_name=prod ${cmd}\""
+  eval "aws-okta ${aopts} exec prod -- ${SHELL} -c \"${cmd}\""
 }
 
 ad () {
   local cmd=${argv:-$SHELL}
-  eval "aws-okta ${aopts} exec dev -- ${SHELL} -c \"auth_name=dev ${cmd}\""
+  eval "aws-okta ${aopts} exec dev -- ${SHELL} -c \"${cmd}\""
 }
 
 al () {
   local cmd=${argv:-$SHELL}
-  eval "aws-okta ${aopts} exec legacy -- ${SHELL} -c \"auth_name=legacy ${cmd}\""
+  eval "aws-okta ${aopts} exec legacy -- ${SHELL} -c \"${cmd}\""
 }
+
+# alias aod='unset AWS_SECRET_KEY_ID && eval $(aws-okta -a 8h -t 8h --mfa-provider GOOGLE --mfa-factor-type token:software:totp env dev)'
+# alias aop='eval $(aws-okta -a 8h -t 8h --mfa-provider GOOGLE --mfa-factor-type token:software:totp env prod)'
+# alias aor='unset AWS_SECRET_KEY_ID && eval $(aws-okta -a 8h -t 8h --mfa-provider GOOGLE --mfa-factor-type token:software:totp env root)'
 
 alias aod='ad ~/bin/aws_set_credentials.sh'
 alias aop='ap ~/bin/aws_set_credentials.sh'
