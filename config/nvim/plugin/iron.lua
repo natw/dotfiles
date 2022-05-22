171762712a795c4663174bf4c5db6854d1fca6ef
Nat Williams <nat.williams@amount.com>
2022-05-22 11:31:29 -0500

    [vim] update iron config for v3

--- config/nvim/plugin/iron.lua
+++ config/nvim/plugin/iron.lua
@@ -1,9 +1,22 @@
-local iron = require('iron')
+local iron = require('iron.core')
 
-iron.core.add_repl_definitions {
-  pgsql = {
-    psql = {
-      command = {"psql"}
+iron.setup {
+  config = {
+    repl_definition = {
+      pgsql = {
+        command = {"psql"}
+      },
     },
   },
+
+  keymaps = {
+    send_motion = "<space>sc",
+    visual_send = "<space>sc",
+    send_line = "<space>sl",
+    repeat_cmd = "<space>s.",
+    cr = "<space>s<cr>",
+    interrupt = "<space>s<space>",
+    exit = "<space>sq",
+    clear = "<space>cl",
+  }
 }
