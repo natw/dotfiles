6228b9ba48d68c86c5abfdc159b836a1f2873136
Nat Williams <nat.williams@gmail.com>
2022-08-29 08:50:29 -0500

    [vim] fix iron-nvim keybindings

--- config/nvim/plugin/iron.lua
+++ config/nvim/plugin/iron.lua
@@ -3,6 +3,9 @@ local iron = require('iron.core')
 iron.setup {
   config = {
     repl_definition = {
+      sql = {
+        command = {"psql"}
+      },
       pgsql = {
         command = {"psql"}
       },
@@ -10,13 +13,16 @@ iron.setup {
   },
 
   keymaps = {
-    send_motion = "<space>sc",
-    visual_send = "<space>sc",
-    send_line = "<space>sl",
-    send_mark = "<space>s.",
-    cr = "<space>s<cr>",
-    interrupt = "<space>s<space>",
-    exit = "<space>sq",
-    clear = "<space>cl",
+    send_motion = "<leader>sc",
+    visual_send = "<leader>sc",
+    send_line = "<leader>sl",
+    send_mark = "<leader>s.",
+    cr = "<leader>s<cr>",
+    interrupt = "<leader>s<space>",
+    exit = "<leader>sq",
+    clear = "<leader>cl",
   }
 }
+
+vim.api.nvim_buf_set_keymap(0, 'v', '<leader>ss', '<cmd>lua require("iron.core").visual_send()<cr>', { noremap=true, silent=true })
+vim.api.nvim_buf_set_keymap(0, 'n', '<leader>ss', '<cmd>lua require("iron.core").send_motion("ip")<cr>', { noremap=true, silent=true })
