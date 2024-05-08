36c0d41fcc6a537cb4ef29272b91cd4d67c5c2f0
Nat Williams <nat.williams@gmail.com>
2024-05-08 11:20:40 -0500

    removed a whole bunch of filetype vim plugins
    I think pretty much everything here is done or should be done
    with tree-sitter

--- config/nvim/lua/plugins/filetypes.lua
+++ config/nvim/lua/plugins/filetypes.lua
@@ -1,51 +1,7 @@
 return {
-  {
-    'tpope/vim-markdown',
-    config = function()
-      vim.g.markdown_fenced_languages = { 'vim', 'go', 'help' }
-    end,
-    ft = { "markdown" },
-  },
-
-  {
-    'elzr/vim-json',
-    config = function()
-      vim.g.vim_json_syntax_conceal = 0
-    end,
-    ft = { "json" },
-  },
-
-  {
-    'hashivim/vim-terraform',
-    ft = 'terraform',
-    config = function()
-      vim.g.terraform_fmt_on_save = 0
-    end,
-  },
-
-  {
-    'pearofducks/ansible-vim',
-    config = function()
-      vim.g.ansible_options = { ignore_blank_lines = 0 }
-    end,
-    -- ft = { "yaml" },
-  },
-
-  -- {
-  --   "AbstractMachinesLab/tree-sitter-sexp",
-  --   -- build = "tree-sitter generate && tree-sitter test",
-  -- },
-
-  { 'cespare/vim-toml', ft = { "toml" } },
-  { 'jparise/vim-graphql', ft = { "graphql" } },
-  { 'pangloss/vim-javascript', ft = { "javascript" } },
-  { 'leafgarland/typescript-vim', ft = { "typescript" } },
+  -- can probably remove some of these, but I'll see next time I actually use these filetypes
   { 'guns/vim-clojure-static', ft = { "clojure" } },
   { 'Olical/conjure', ft = { 'clojure' } },
   { 'eraserhd/parinfer-rust', ft = { "clojure", "lisp" }, build = 'cargo build --release' },
-  { 'vim-python/python-syntax', ft = { 'python' } },
-  { 'exu/pgsql.vim', ft = { 'sql' } },
   { 'lervag/vimtex', ft = { 'tex', 'latex' } },
-  { 'othree/html5.vim', ft = 'html' },
-  { 'vim-ruby/vim-ruby', ft = 'ruby' },
 }
