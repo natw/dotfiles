98676635615b40cd9784535a44e1667ab70482b0
Nat Williams <nat.williams@gmail.com>
2024-07-10 10:42:42 -0500

    copy fzf bits from other file

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,4 +1,18 @@
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
+
+###
 
 export FZF_DEFAULT_COMMAND='ag -g ""'
 
