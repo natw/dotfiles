4cca274b7bc859df4144e9224d9254f7f1eb3f69
Nat Williams <nat.williams@gmail.com>
2025-03-28 10:01:56 -0500

    terraformls - ignore single file warning

--- config/nvim/lsp/terraformls.lua
+++ config/nvim/lsp/terraformls.lua
@@ -2,12 +2,15 @@ return {
   cmd = { "terraform-ls", "serve" },
   filetypes = { "terraform", "terraform-vars" },
   root_markers = { ".terraform" },
+  init_options = {
+    ignoreSingleFileWarning = true,
+  },
   settings = {
     experimentalFeatures = {
       prefillRequiredField = true,
     },
   },
-  on_attach = function(client, bufnr)
+  on_attach = function(client, _)
     client.server_capabilities.semanticTokensProvider = nil
   end,
 }
